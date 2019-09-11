c514c6cda4aed5962f3dbb87581984a146aeb182
marcos <marcos@MARCOS-PC>
2019-09-11 14:38:12 -0400

    Modificado Clase hola Mundo

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -2,8 +2,8 @@
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hola Mundo...");
+		
+		System.out.println("Hola Mundo...2");
 	}
 
 }
